f6d0bd8fba4ffaa42249ce55a1f854a9efe245ca
Minwoo Lee <ermaker@gmail.com>
2015-12-30 22:18:07 +0900

    Modify account

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ require 'xingAPI/api'
 
 class App < Sinatra::Application
   configure do
-    @api = XingAPI::API
+    @@api = XingAPI::API.new(
+      ENV['IP'], ENV['PORT'],
+      ENV['ID'], ENV['PASS'], ENV['PASS2'])
   end
 
   get '/account/:idx' do |idx|
-    XingAPI::API.new(ENV['IP'], ENV['PORT'], ENV['ID'], ENV['PASS'], ENV['PASS2']) do |api|
-      @account = XingAPI::XingAPI.account(idx.to_i)
-    end
+    @account = @@api.account(idx.to_i)
     jbuilder 'json.response @account'
   end
 end
