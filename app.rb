af415b914b137e4c4d80104fbf89dbe1edcd29fb
Minwoo Lee <ermaker@gmail.com>
2016-01-12 03:00:30 +0900

    Add t1903

--- app.rb
+++ app.rb
@@ -14,30 +14,34 @@ class App < Sinatra::Application
     jbuilder 'json.response @account'
   end
 
+  def to_symbol(value)
+    case value
+    when Array
+      value.map { |v| to_symbol(v) }
+    when Hash
+      Hash[value.map { |key, value| [key.to_sym, to_symbol(value)] }]
+    else
+      value
+    end
+  end
+
   post '/tr/:tr_name' do |tr_name|
     request.body.rewind
-    params = MultiJson.load(request.body.string)
+    params = to_symbol(MultiJson.load(request.body.string))
     case tr_name
-    when 't1901'
-      @result = @@api.tr_t1901(params['shcode'].to_s)
-      jbuilder <<-EOJ
-        json.response @result['data']
-        json.(@result, 'message')
-      EOJ
     when 'CSPAT00600'
       @result = @@api.tr_CSPAT00600(
-        params['account'].to_s,
-        params['pass'].to_s,
-        params['shcode'].to_s,
-        params['qty'],
-        params['sell_or_buy'].to_sym,
+        params[:account].to_s,
+        params[:pass].to_s,
+        params[:shcode].to_s,
+        params[:qty],
+        params[:sell_or_buy].to_sym,
         )
-      jbuilder <<-EOJ
-        json.response @result['data']
-        json.(@result, 'message')
-      EOJ
     else
-      abort
+      @result = @@api.tr(tr_name, **params)
     end
+    jbuilder <<-EOJ
+      json.(@result, 'response', 'message')
+    EOJ
   end
 end
