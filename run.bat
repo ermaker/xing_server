6efe7282c19e456ba2eced3168c7b9759f5d4043
Minwoo Lee <ermaker@gmail.com>
2016-07-02 16:00:11 +0900

    Add script support

--- run.bat
+++ run.bat
@@ -4,5 +4,8 @@ git stash -u
 git pull --rebase=preserve origin HEAD
 git stash pop
 
-bundle update xingAPI
-bundle exec rake server >log.txt 2>log2.txt
+REM call bundle update xingAPI
+REM call gem uninstall ffi -Ia
+REM call gem install ffi --platform ruby
+
+call bundle exec rake server >log.txt 2>log2.txt
