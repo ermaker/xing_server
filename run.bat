381f4c68e2e738888e10fb35171f0ba21fb8585d
Minwoo Lee <ermaker@gmail.com>
2016-07-02 18:04:57 +0900

    Update script

--- run.bat
+++ run.bat
@@ -4,8 +4,7 @@ git stash -u
 git pull --rebase=preserve origin HEAD
 git stash pop
 
-REM call bundle update xingAPI
-REM call gem uninstall ffi -Ia
-REM call gem install ffi --platform ruby
+call bundle update xingAPI
+bundle exec ruby -rffi -e '' 2>NUL || call gem uninstall ffi -Ia && call gem install ffi --platform ruby
 
 call bundle exec rake server >log.txt 2>log2.txt
