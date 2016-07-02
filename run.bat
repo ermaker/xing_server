7873b2a5284923154f18c54868da2d624592f48b
Minwoo Lee <ermaker@gmail.com>
2016-07-02 18:15:22 +0900

    Remove unused code

--- run.bat
+++ run.bat
@@ -5,9 +5,6 @@ git pull --rebase=preserve origin HEAD
 git stash pop
 
 call bundle update xingAPI
-echo TEST
 call bundle exec ruby -rffi -e '' 2>NUL || call gem uninstall ffi -Ia && call gem install ffi --platform ruby
 
-echo TEST
 call bundle exec rake server >log.txt 2>log2.txt
-echo TEST
